773e48367066a7f649c6f2c613393d911aafa2d2
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-07-29 17:48:49 +0530

    Add files via upload

---
+++ WorkingWithPropertyPaneControls/src/webparts/propertyPaneControlsDemos/PropertyPaneControlsDemosWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./PropertyPaneControlsDemosWebPart.module.css");
+const styles = {
+  propertyPaneControlsDemos: 'propertyPaneControlsDemos_08318428',
+  container: 'container_08318428',
+  row: 'row_08318428',
+  column: 'column_08318428',
+  'ms-Grid': 'ms-Grid_08318428',
+  title: 'title_08318428',
+  subTitle: 'subTitle_08318428',
+  description: 'description_08318428',
+  button: 'button_08318428',
+  label: 'label_08318428'
+};
+
+export default styles;
+/* tslint:enable */
