673bd5f4443774dbcfaf337d8d1d2074810ce765
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-07-29 17:50:30 +0530

    Add files via upload

---
+++ ListCreationDemo/src/webparts/newListCreationWp/NewListCreationWpWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./NewListCreationWpWebPart.module.css");
+const styles = {
+  newListCreationWp: 'newListCreationWp_914500d1',
+  container: 'container_914500d1',
+  row: 'row_914500d1',
+  column: 'column_914500d1',
+  'ms-Grid': 'ms-Grid_914500d1',
+  title: 'title_914500d1',
+  subTitle: 'subTitle_914500d1',
+  description: 'description_914500d1',
+  button: 'button_914500d1',
+  label: 'label_914500d1'
+};
+
+export default styles;
+/* tslint:enable */
---
+++ SiteCreationDemo/src/webparts/siteCreationWp/SiteCreationWpWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./SiteCreationWpWebPart.module.css");
+const styles = {
+  siteCreationWp: 'siteCreationWp_cfd4fd80',
+  container: 'container_cfd4fd80',
+  row: 'row_cfd4fd80',
+  column: 'column_cfd4fd80',
+  'ms-Grid': 'ms-Grid_cfd4fd80',
+  title: 'title_cfd4fd80',
+  subTitle: 'subTitle_cfd4fd80',
+  description: 'description_cfd4fd80',
+  button: 'button_cfd4fd80',
+  label: 'label_cfd4fd80'
+};
+
+export default styles;
+/* tslint:enable */
