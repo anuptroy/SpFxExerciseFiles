fed0201f82f5326e7a5e37464ac0f22741e58bcd
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-07-29 17:46:38 +0530

    Add files via upload

---
+++ GetListOfAllListsDemo/src/webparts/getlistofalllists/GetlistofalllistsWebPart.module.scss.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+require("./GetlistofalllistsWebPart.module.css");
+const styles = {
+  getlistofalllists: 'getlistofalllists_891d3214',
+  teams: 'teams_891d3214',
+  welcome: 'welcome_891d3214',
+  welcomeImage: 'welcomeImage_891d3214',
+  links: 'links_891d3214'
+};
+
+export default styles;
+/* tslint:enable */
