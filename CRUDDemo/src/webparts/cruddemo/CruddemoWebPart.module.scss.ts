026dae9b424255be977378725b4e699181c6c0e7
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-07-29 17:51:03 +0530

    Add files via upload

---
+++ CRUDDemo/src/webparts/cruddemo/CruddemoWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./CruddemoWebPart.module.css");
+const styles = {
+  cruddemo: 'cruddemo_73e15137',
+  container: 'container_73e15137',
+  row: 'row_73e15137',
+  column: 'column_73e15137',
+  'ms-Grid': 'ms-Grid_73e15137',
+  title: 'title_73e15137',
+  subTitle: 'subTitle_73e15137',
+  description: 'description_73e15137',
+  button: 'button_73e15137',
+  label: 'label_73e15137'
+};
+
+export default styles;
+/* tslint:enable */
