180074ff094a2d76218147e3dcc2718590c36052
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-07-29 17:48:17 +0530

    Add files via upload

---
+++ ReadSitePropertiesWebPart/src/webparts/readSitePropertiesWebPart/ReadSitePropertiesWebPartWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./ReadSitePropertiesWebPartWebPart.module.css");
+const styles = {
+  readSitePropertiesWebPart: 'readSitePropertiesWebPart_d0eb50b7',
+  container: 'container_d0eb50b7',
+  row: 'row_d0eb50b7',
+  column: 'column_d0eb50b7',
+  'ms-Grid': 'ms-Grid_d0eb50b7',
+  title: 'title_d0eb50b7',
+  subTitle: 'subTitle_d0eb50b7',
+  description: 'description_d0eb50b7',
+  button: 'button_d0eb50b7',
+  label: 'label_d0eb50b7'
+};
+
+export default styles;
+/* tslint:enable */
