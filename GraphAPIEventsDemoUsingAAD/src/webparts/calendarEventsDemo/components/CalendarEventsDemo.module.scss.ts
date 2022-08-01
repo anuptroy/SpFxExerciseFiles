6ceca484a623d47f17dfe728a2fb6f4335753f52
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:32:40 +0530

    Add files via upload

---
+++ GraphAPIEventsDemoUsingAAD/src/webparts/calendarEventsDemo/components/CalendarEventsDemo.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./CalendarEventsDemo.module.css");
+const styles = {
+  calendarEventsDemo: 'calendarEventsDemo_65d12b10',
+  container: 'container_65d12b10',
+  row: 'row_65d12b10',
+  column: 'column_65d12b10',
+  'ms-Grid': 'ms-Grid_65d12b10',
+  title: 'title_65d12b10',
+  subTitle: 'subTitle_65d12b10',
+  description: 'description_65d12b10',
+  button: 'button_65d12b10',
+  label: 'label_65d12b10'
+};
+
+export default styles;
+/* tslint:enable */
