790f58016963063f23d9f500eed43cbeea0daa93
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:39:51 +0530

    Add files via upload

---
+++ DeploymentDemos/HostingThroughLocalServerDemo/src/webparts/htlsDemo/HtlsDemoWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./HtlsDemoWebPart.module.css");
+const styles = {
+  htlsDemo: 'htlsDemo_2dadcbf7',
+  container: 'container_2dadcbf7',
+  row: 'row_2dadcbf7',
+  column: 'column_2dadcbf7',
+  'ms-Grid': 'ms-Grid_2dadcbf7',
+  title: 'title_2dadcbf7',
+  subTitle: 'subTitle_2dadcbf7',
+  description: 'description_2dadcbf7',
+  button: 'button_2dadcbf7',
+  label: 'label_2dadcbf7'
+};
+
+export default styles;
+/* tslint:enable */
