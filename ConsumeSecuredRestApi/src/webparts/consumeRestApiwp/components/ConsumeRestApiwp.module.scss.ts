0f6508c94dc6218b8c518d1381dd95c7adf3889e
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:33:39 +0530

    Add files via upload

---
+++ ConsumeSecuredRestApi/src/webparts/consumeRestApiwp/components/ConsumeRestApiwp.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./ConsumeRestApiwp.module.css");
+const styles = {
+  consumeRestApiwp: 'consumeRestApiwp_500fe2c0',
+  container: 'container_500fe2c0',
+  row: 'row_500fe2c0',
+  column: 'column_500fe2c0',
+  'ms-Grid': 'ms-Grid_500fe2c0',
+  title: 'title_500fe2c0',
+  subTitle: 'subTitle_500fe2c0',
+  description: 'description_500fe2c0',
+  button: 'button_500fe2c0',
+  label: 'label_500fe2c0'
+};
+
+export default styles;
+/* tslint:enable */
