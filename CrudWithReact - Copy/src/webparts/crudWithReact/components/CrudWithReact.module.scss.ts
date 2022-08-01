18d287ce47cc323cf79f5eb894ca380f24272937
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:27:03 +0530

    Add files via upload

---
+++ CrudWithReact - Copy/src/webparts/crudWithReact/components/CrudWithReact.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./CrudWithReact.module.css");
+const styles = {
+  crudWithReact: 'crudWithReact_f6785fe5',
+  container: 'container_f6785fe5',
+  row: 'row_f6785fe5',
+  column: 'column_f6785fe5',
+  'ms-Grid': 'ms-Grid_f6785fe5',
+  title: 'title_f6785fe5',
+  subTitle: 'subTitle_f6785fe5',
+  description: 'description_f6785fe5',
+  button: 'button_f6785fe5',
+  label: 'label_f6785fe5'
+};
+
+export default styles;
+/* tslint:enable */
---
+++ CrudWithReact/src/webparts/crudWithReact/components/CrudWithReact.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./CrudWithReact.module.css");
+const styles = {
+  crudWithReact: 'crudWithReact_8fcbb454',
+  container: 'container_8fcbb454',
+  row: 'row_8fcbb454',
+  column: 'column_8fcbb454',
+  'ms-Grid': 'ms-Grid_8fcbb454',
+  title: 'title_8fcbb454',
+  subTitle: 'subTitle_8fcbb454',
+  description: 'description_8fcbb454',
+  button: 'button_8fcbb454',
+  label: 'label_8fcbb454'
+};
+
+export default styles;
+/* tslint:enable */
