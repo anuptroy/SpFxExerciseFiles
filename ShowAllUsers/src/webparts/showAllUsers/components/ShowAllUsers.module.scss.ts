258f1d9d4c5b174a98716be68e4dfe2548d78745
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:24:07 +0530

    Add files via upload

---
+++ ShowAllUsers/src/webparts/showAllUsers/components/ShowAllUsers.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./ShowAllUsers.module.css");
+const styles = {
+  showAllUsers: 'showAllUsers_c43f2493',
+  container: 'container_c43f2493',
+  row: 'row_c43f2493',
+  column: 'column_c43f2493',
+  'ms-Grid': 'ms-Grid_c43f2493',
+  title: 'title_c43f2493',
+  subTitle: 'subTitle_c43f2493',
+  description: 'description_c43f2493',
+  button: 'button_c43f2493',
+  label: 'label_c43f2493'
+};
+
+export default styles;
+/* tslint:enable */
