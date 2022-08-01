a69c50151cb42fc0bf3fff75ec816289d76f2e52
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:22:29 +0530

    Add files via upload

---
+++ AnonymousAPIDemo/src/webparts/anonymousApiwpDemo/components/AnonymousApiwpDemo.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./AnonymousApiwpDemo.module.css");
+const styles = {
+  anonymousApiwpDemo: 'anonymousApiwpDemo_ae952902',
+  container: 'container_ae952902',
+  row: 'row_ae952902',
+  column: 'column_ae952902',
+  'ms-Grid': 'ms-Grid_ae952902',
+  title: 'title_ae952902',
+  subTitle: 'subTitle_ae952902',
+  description: 'description_ae952902',
+  button: 'button_ae952902',
+  label: 'label_ae952902'
+};
+
+export default styles;
+/* tslint:enable */
---
+++ AnonymousAPIDemo2/src/webparts/anonymousDemo2/components/AnonymousDemo2.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./AnonymousDemo2.module.css");
+const styles = {
+  anonymousDemo2: 'anonymousDemo2_6c5a4bee',
+  container: 'container_6c5a4bee',
+  row: 'row_6c5a4bee',
+  column: 'column_6c5a4bee',
+  'ms-Grid': 'ms-Grid_6c5a4bee',
+  title: 'title_6c5a4bee',
+  subTitle: 'subTitle_6c5a4bee',
+  description: 'description_6c5a4bee',
+  button: 'button_6c5a4bee',
+  label: 'label_6c5a4bee'
+};
+
+export default styles;
+/* tslint:enable */
