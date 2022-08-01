ee7e59c3ff4e04aa34e0072faf21a9d03e409de5
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:38:41 +0530

    Add files via upload

---
+++ MicrosoftTeamsTabDemo/src/webparts/microsoftTeamsTab/MicrosoftTeamsTabWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./MicrosoftTeamsTabWebPart.module.css");
+const styles = {
+  microsoftTeamsTab: 'microsoftTeamsTab_dc47fd7f',
+  container: 'container_dc47fd7f',
+  row: 'row_dc47fd7f',
+  column: 'column_dc47fd7f',
+  'ms-Grid': 'ms-Grid_dc47fd7f',
+  title: 'title_dc47fd7f',
+  subTitle: 'subTitle_dc47fd7f',
+  description: 'description_dc47fd7f',
+  button: 'button_dc47fd7f',
+  label: 'label_dc47fd7f'
+};
+
+export default styles;
+/* tslint:enable */
