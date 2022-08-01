2524622959b17c3ea6abe3ef4ffa03ac6a61927f
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:24:50 +0530

    Add files via upload

---
+++ GraphAPIUPDemo/src/webparts/userProfileInfo/UserProfileInfoWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./UserProfileInfoWebPart.module.css");
+const styles = {
+  userProfileInfo: 'userProfileInfo_7fc95050',
+  container: 'container_7fc95050',
+  row: 'row_7fc95050',
+  column: 'column_7fc95050',
+  'ms-Grid': 'ms-Grid_7fc95050',
+  title: 'title_7fc95050',
+  subTitle: 'subTitle_7fc95050',
+  description: 'description_7fc95050',
+  button: 'button_7fc95050',
+  label: 'label_7fc95050'
+};
+
+export default styles;
+/* tslint:enable */
