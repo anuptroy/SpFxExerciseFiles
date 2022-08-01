23f2ef1a5a3e821c2bc0f8889da8e3dc649ef947
Anup T Roy <40425348+anuptroy@users.noreply.github.com>
2022-08-01 16:23:10 +0530

    Add files via upload

---
+++ ExternalLibrariesDemo/src/webparts/extLibDemo/ExtLibDemoWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require("./ExtLibDemoWebPart.module.css");
+const styles = {
+  extLibDemo: 'extLibDemo_5bdc48b6',
+  container: 'container_5bdc48b6',
+  row: 'row_5bdc48b6',
+  column: 'column_5bdc48b6',
+  'ms-Grid': 'ms-Grid_5bdc48b6',
+  title: 'title_5bdc48b6',
+  subTitle: 'subTitle_5bdc48b6',
+  description: 'description_5bdc48b6',
+  button: 'button_5bdc48b6',
+  label: 'label_5bdc48b6'
+};
+
+export default styles;
+/* tslint:enable */
